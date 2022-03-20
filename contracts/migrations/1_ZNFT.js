d8f842d14a6b93848c0e6db261c4ced6603b8789
yafantao <765370813@qq.com>
2022-03-20 12:44:40 +0800

    Add saving address logic

--- contracts/migrations/1_ZNFT.js
+++ contracts/migrations/1_ZNFT.js
@@ -1,5 +1,7 @@
 const ZNFT = artifacts.require("ZNFT");
+const {saveToJson} = require('../utils/jsonRW.js');
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network) {
   await deployer.deploy(ZNFT);
+  saveToJson("ZNFT", ZNFT.address, network);
 };
